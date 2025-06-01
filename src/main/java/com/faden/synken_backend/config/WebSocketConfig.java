4470b3d5f6e84e852ae1631c698f49a64bdc3858
João Victor <jvictordomingos123@gmail.com>
2025-06-01 18:55:59 -0300

    Correções

--- src/main/java/com/faden/synken_backend/config/WebSocketConfig.java
+++ src/main/java/com/faden/synken_backend/config/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package com.faden.synken_backend.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -10,6 +11,8 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
+    @Value("${api.web.url}")
+    private String url;
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
@@ -21,6 +24,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws-chat")
-                .setAllowedOrigins("http://localhost:5173");
+                .setAllowedOrigins(url);
     }
 }
--- src/main/java/com/faden/synken_backend/controllers/MessageController.java
+++ src/main/java/com/faden/synken_backend/controllers/MessageController.java
@@ -1,12 +1,10 @@
 package com.faden.synken_backend.controllers;
 
 import com.faden.synken_backend.dtos.MessageDTO;
-import com.faden.synken_backend.models.Message;
 import com.faden.synken_backend.services.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
 @Controller
